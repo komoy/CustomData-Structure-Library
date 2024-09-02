33943706e506a25dbed4a8c187f1f165a59b4613
tevin taylor <tevintaylor_17@yahoo.com>
2024-09-02 17:37:40 -0500

    function to insert item to array Property to get the length of the array Method to print the array contents

--- CustomArray.cs
+++ CustomArray.cs
@@ -20,5 +20,26 @@
             //Check for outofRange
 
         }
+
+        // function to insert item to array
+        public void Add(T item)
+        {
+            _array[_count++] = item;
+        }
+
+        // Property to get the length of the array
+        public int Length
+        {
+            get { return _array.Length; }
+        }
+
+        // Method to print the array contents
+        public void Print()
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                Console.WriteLine($"Element at index {i}: {_array[i]}");
+            }
+        }
     }
 }
