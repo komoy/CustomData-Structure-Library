ec5abe8f05a7d23311c636e9a508a945047c8159
komoy haye <komoyhaye16@gmail.com>
2024-09-03 11:40:28 -0500

    add summary

--- CustomLinkedList.cs
+++ CustomLinkedList.cs
@@ -17,6 +17,11 @@ namespace Custom_Data_Structure_Library
             this.Next = null;
         }
     }
+    /// <summary>
+    ///     A linked list is a data structure that consist of a sequence of element, where each element (often call a node ) 
+    ///     contain a value and a reference to the next element in the sequence.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class CustomLinkedList<T>
     {
         private Node<T> _head;
