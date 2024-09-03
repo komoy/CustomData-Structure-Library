45807080a98925554abf9812397dbde7a25bcc8c
komoy haye <komoyhaye16@gmail.com>
2024-09-03 11:38:00 -0500

    add constructor

--- CustomLinkedList.cs
+++ CustomLinkedList.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Custom_Data_Structure_Library
 {
+
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+        public Node(T data)
+        {
+            this.Data = data;
+            this.Next = null;
+        }
+    }
     public class CustomLinkedList<T>
     {
+        private Node<T> _head;
+        public CustomLinkedList() 
+        {
+            _head = null;
+        }
     }
 }
