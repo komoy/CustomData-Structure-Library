8531ca4703891eb09d0bba280d458c4d52c39bf4
komoy haye <komoyhaye16@gmail.com>
2024-09-03 15:24:08 -0500

    add skeleton for methods

--- CustomStack.cs
+++ CustomStack.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,26 @@ namespace Custom_Data_Structure_Library
 {
     public class CustomStack<T>
     {
+        private T[] _stack;
+        private int _top;
+        public CustomStack(int size) 
+        { 
+            _stack = new T[size];
+            _top = -1;
+        }
+
+        public void Pop()
+        {
+
+        }
+        public void Push(T value)
+        {
+
+        }
+
+        public T Peek()
+        {
+            return _stack[_top];// to do 
+        }
     }
 }
