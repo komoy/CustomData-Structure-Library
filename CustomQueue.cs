a31da63cf806e30db682e90cd77172908de9a8f4
tevin taylor <tevintaylor_17@yahoo.com>
2024-09-04 16:44:15 -0500

    queue final implementation

--- CustomQueue.cs
+++ CustomQueue.cs
@@ -10,8 +10,9 @@ namespace Custom_Data_Structure_Library
     {
         private T[] _queue;    // Array to store queue elements
         private int _front;    // Index of the front element in the queue
-        private int _back;     // Index where the next element will be added
-        private int count;     // Number of elements currently in the queue
+        private int _back;
+        private int _count;    // Number of elements currently in the queue
+        private int _size;     // Maximum size of the queue
 
         /// <summary>
         /// Initializes a new instance of the CustomQueue class with the specified size.
@@ -20,9 +21,10 @@ namespace Custom_Data_Structure_Library
         public CustomQueue(int size)
         {
             _queue = new T[size]; // Create an array of the specified size
-            _front = -1;          // Initialize the front index to -1 (queue is empty)
-            _back = 0;            // Initialize the back index to 0 (first position)
-            count = 0;            // Initialize the count to 0 (no elements in the queue)
+            _front = 0;          // Initialize the front index to -1 (queue is empty)
+            _back = 0;
+            _count = 0;           // Initialize the count to 0 (no elements in the queue)
+            _size = size;         // Set the maximum size of the queue
         }
 
         /// <summary>
@@ -31,34 +33,93 @@ namespace Custom_Data_Structure_Library
         /// <param name="item">The item to be added to the queue.</param>
         public void Add(T item)
         {
-            // Implementation for adding an item will go here
+            // Check if the queue is full
+            if (this._count == this._size)
+            {
+                Console.WriteLine("Queue is full"); // Notify that the queue is full
+            }
+            else
+            {
+                // If this is the first element being added, initialize _front to 0
+                if (_front == -1)
+                {
+                    _front = 0;
+                }
+
+                // Add the new item to the back of the queue
+                _queue[_back] = item;
+
+                // Move the back index forward, wrapping around to the start if necessary (circular queue logic)
+                _back = (_back + 1) % _queue.Length;
+
+                // Increase the count of elements in the queue
+                _count++;
+
+            }
         }
 
         /// <summary>
-        /// Initializes the front element of the queue.
+        /// Returns the current number of elements in the queue.
         /// </summary>
-        /// <returns>The element at the front of the queue.</returns>
-        public T Element()
+        /// <returns>The number of elements in the queue.</returns>
+        public int Count()
         {
-            // Implementation for initializing the front element will go here
+            return _count; // Return the number of elements in the queue
         }
 
         /// <summary>
         /// Returns the element at the front of the queue without removing it.
         /// </summary>
         /// <returns>The element at the front of the queue.</returns>
-        public T Peek()
+        public T Element()
         {
-            // Implementation for peeking at the front element will go here
+            return this._queue[this._front]; // Return the front element of the queue
         }
 
         /// <summary>
         /// Removes and returns the element at the front of the queue.
         /// </summary>
         /// <returns>The element at the front of the queue.</returns>
-        public T Poll()
+        public T Dequeue()
         {
-            // Implementation for removing the front element will go here
+            // Check if the queue is empty
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty"); // Throw an exception if the queue is empty
+            }
+
+            // Get the front element of the queue
+            T item = _queue[_front];
+
+            // Move the front index forward (circular queue logic)
+            _front = (_front + 1) % _queue.Length;
+
+            // Decrease the count of elements in the queue
+            _count--;
+
+            // Return the dequeued element
+            return item;
+        }
+
+        public void Print()
+        {
+            int count = this._count;  // Initialize a local variable to keep track of the number of elements left to print
+            int currentIndex = _front;  // Start from the front of the queue
+
+            // Loop until all elements have been printed
+            while (count > 0)
+            {
+                // Print the element at the current index in the queue
+                Console.WriteLine(_queue[currentIndex]);
+
+
+
+                // Move to the next element in the queue (circular logic)
+                currentIndex = (currentIndex + 1) % _queue.Length;
+
+                // Decrement the count to move to the next element
+                count--;
+            }
         }
     }
 }
