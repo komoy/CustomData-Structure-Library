995676041bffd37111e574b5b4258107c981501c
komoy haye <komoyhaye16@gmail.com>
2024-09-04 16:44:59 -0500

    add constructor

--- CustomQueue.cs
+++ CustomQueue.cs
@@ -8,5 +8,23 @@ namespace Custom_Data_Structure_Library
 {
     public class CustomQueue<T>
     {
+        private T[] _queue;
+        private int _count;
+        private int _rear;
+        private int _front;
+
+        public CustomQueue(int size)
+        {
+            _count = 0;
+            _queue = new T[size];
+            _rear = -1;
+            _front= 0;
+
+        }
+
+        public void Add(T item) 
+        {
+
+        }
     }
 }
